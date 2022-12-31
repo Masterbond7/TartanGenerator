9cf7f35da6f0d4ec46189eaa08dd83323213ee82
Masterbond7 <jamesslough4@gmail.com>
2022-12-31 20:39:15 +0000

    Get base colour for columns

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 first_square="U" # Does the thread across go under or over on the column of the first row
 creation_mode="Q" # Is the tartan design file for a full 'tile' or a 'quadrant'?
+colour_dictionary = {"G": (127, 127, 127), "B": (0, 0, 0), "W": (255, 255, 255), "R": (255, 0, 0)}
 
 tartan_design_file = open("tartan_design.csv", 'r')
 tartan_column = tartan_design_file.readline().replace('\n', '').split(',')
@@ -13,4 +14,11 @@ if creation_mode=="Q":
     tartan_row += tartan_row[::-1]
 
 tartan_image = Image.new('RGB', (len(tartan_column), len(tartan_row)), (255,0,255))
+tartan_pixels = tartan_image.load()
+
+# Set columns with the correct base colour
+for i in range(len(tartan_column)):
+    for j in range(len(tartan_row)):
+        tartan_pixels[i,j] = colour_dictionary[tartan_column[i]]
+
 tartan_image.save("tartan_image.png")
