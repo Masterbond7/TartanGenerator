18028ce3b532d80b2ee5c4443cddc3171678a41d
Masterbond7 <jamesslough4@gmail.com>
2022-12-31 20:15:13 +0000

    Write simple code to parse tartan_design file

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+first_square="U" # Does the thread across go under or over on the column of the first row
+
+tartan_design_file = open("tartan_design.csv", 'r')
+tartan_row = tartan_design_file.readline().replace('\n', '').split(',')
+tartan_column = tartan_design_file.readline().replace('\n', '').split(',')
+
+print(tartan_row, '\n', tartan_column)
