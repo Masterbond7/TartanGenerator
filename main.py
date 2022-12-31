afd218ae35e4db5115a20c0809af5b543748d013
Masterbond7 <jamesslough4@gmail.com>
2022-12-31 20:20:47 +0000

    Make png size of tartan pattern

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from PIL import Image
+
 first_square="U" # Does the thread across go under or over on the column of the first row
 
 tartan_design_file = open("tartan_design.csv", 'r')
-tartan_row = tartan_design_file.readline().replace('\n', '').split(',')
 tartan_column = tartan_design_file.readline().replace('\n', '').split(',')
+tartan_row = tartan_design_file.readline().replace('\n', '').split(',')
+tartan_design_file.close()
 
-print(tartan_row, '\n', tartan_column)
+tartan_image = Image.new('RGB', (len(tartan_column), len(tartan_row)), (255,0,255))
+tartan_image.save("tartan_image.png")
